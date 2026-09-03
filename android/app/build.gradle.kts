3ddcabdaa4e1d925a2231ab386d4c33326e1fe94
anaf-4 <kimdongmin12355@gmail.com>
2026-09-03 20:28:18 +0900

    chore: release v1.0.10

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.personalcalendar.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "1.0.9"
+        versionCode = 9
+        versionName = "1.0.10"
 
         buildConfigField("String", "GITHUB_REPO", "\"anaf-4/personal-calendar\"")
     }
